a2820099041f78504f9591bf4b116f67a965f759
Mehmet Ali Karabulut <mehmetalikarabulutt@gmail.com>
2023-09-24 16:11:37 +0300

    Initial commit

---
+++ Models/ClientViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+namespace NetCoreProject.Models
+{
+    public class ClientViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Lastname { get; set; }
+        public int Age { get; set; }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace NetCoreProject.Models;
+
+public class ErrorViewModel
+{
+    public string RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
